96cfd97f1c57252d47dfe057a00c83bd297f61c6
WassiW80 <sayedwaseeyat111@gmail.com>
2020-07-26 18:11:02 +0530

    ADDED : Login and signup page is added.

--- app/src/main/java/com/example/pathology/signup.java
+++ app/src/main/java/com/example/pathology/signup.java
@@ -3,17 +3,12 @@ package com.example.pathology;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar tool_bar = findViewById(R.id.tool_bar);
-        setSupportActionBar();
-
+        setContentView(R.layout.activity_signup);
     }
 }
