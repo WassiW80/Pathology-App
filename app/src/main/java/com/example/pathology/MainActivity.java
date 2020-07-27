52b0fc3912ff3d43feaa495ad8967dfa5685ef3b
WassiW80 <sayedwaseeyat111@gmail.com>
2020-07-27 16:25:04 +0530

    Refactored: Layout error fixed.

--- app/src/main/java/com/example/pathology/MainActivity.java
+++ app/src/main/java/com/example/pathology/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar tool_bar = findViewById(R.id.tool_bar);
-        setSupportActionBar();
+//        Toolbar tool_bar = findViewById(R.id.tool_bar);
+       // setSupportActionBar();
 
     }
 }
