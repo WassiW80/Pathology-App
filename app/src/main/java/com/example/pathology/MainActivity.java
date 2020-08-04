df89db6bc7ef1859b0fa30c8cf9f78ace392015e
Sandy-07 <mr.shaikh1998@gmail.com>
2020-08-04 21:38:06 +0530

    ADDED: Navbar Login Singup and menubar page.

--- app/src/main/java/com/example/pathology/MainActivity.java
+++ app/src/main/java/com/example/pathology/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.pathology;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
-
+import android.view.MenuItem;
+import android.view.View;
+import androidx.appcompat.widget.Toolbar;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +26,49 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView = findViewById(R.id.navigation_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                UserMenuSelected(menuItem);
+                return false;
+            }
+        });
+
     }
 
-    private void setSupportActionBar(Toolbar toolbar) {
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    public void UserMenuSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_reports:
+                break;
+
+            case R.id.nav_upload:
+                break;
+
+            case R.id.nav_add:
+                break;
+
+            case R.id.nav_view:
+                break;
+
+            case R.id.nav_settings:
+                break;
+
+            case R.id.nav_contact:
+                break;
+        }
     }
 }
